049025052ff126aae213562ec33a7ed6a92e08d5
hidayat.febiansyah <hidayat.febiansyah@gmail.com>
2017-02-10 11:15:45 +0700

    hole and player test

--- tests/com/havban/congklak/models/impl/DefaultHoleTest.java
+++ tests/com/havban/congklak/models/impl/DefaultHoleTest.java
@@ -1,5 +1,8 @@
 package com.havban.congklak.models.impl;
 
+import com.havban.congklak.models.Hole;
+import com.havban.congklak.models.Player;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,39 +11,59 @@ import static org.junit.Assert.*;
  * Created by hidayat.febiansyah on 2/10/17.
  */
 public class DefaultHoleTest {
+
+    final static int SEEDS = 1;
+    final static int BOARD_SIZE = 2;
+
+    final static String TO_STRING = ""+SEEDS;
+
+    Hole hole;
+
+    @Before
+    public void before(){
+        hole = new DefaultHole(SEEDS, BOARD_SIZE);
+    }
+
     @Test
     public void getNumberOfSeed() throws Exception {
-
+        assert(hole.getNumberOfSeed() == SEEDS);
     }
 
     @Test
     public void incSeed() throws Exception {
-
+        hole.incSeed();
+        assert(hole.getNumberOfSeed() == SEEDS+1);
     }
 
     @Test
     public void takeAllSeed() throws Exception {
-
+        hole.takeAllSeed();
+        assert(hole.getNumberOfSeed() == 0);
     }
 
     @Test
     public void addSeed() throws Exception {
-
+        hole.addSeed(SEEDS);
+        assert(hole.getNumberOfSeed() == SEEDS+SEEDS);
     }
 
     @Test
     public void toStringTest() throws Exception {
-
+        assert(hole.toString().equals(TO_STRING));
     }
 
     @Test
     public void setKacang() throws Exception {
-
+        hole.setKacang(false);
+        assert(hole.isKacang() == false);
+        hole.setKacang(true);
+        assert(hole.isKacang() == true);
     }
 
     @Test
     public void isKacang() throws Exception {
-
+        hole.setKacang(true);
+        assert(hole.isKacang() == true);
     }
 
 }
--- tests/com/havban/congklak/models/impl/DefaultPlayerTest.java
+++ tests/com/havban/congklak/models/impl/DefaultPlayerTest.java
@@ -1,5 +1,7 @@
 package com.havban.congklak.models.impl;
 
+import com.havban.congklak.models.Player;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,24 +10,38 @@ import static org.junit.Assert.*;
  * Created by hidayat.febiansyah on 2/10/17.
  */
 public class DefaultPlayerTest {
+
+    final static int PLAYER_ID = 1;
+    final static String PLAYER_NAME = "NAME1";
+
+    final static String TO_STRING = "Player "+PLAYER_ID+" - "+PLAYER_NAME;
+
+    Player p;
+
+    @Before
+    public void before(){
+        p = new DefaultPlayer(PLAYER_ID, PLAYER_NAME);
+    }
+
     @Test
     public void getName() throws Exception {
-
+        assert(p.getName().equals(PLAYER_NAME));
     }
 
     @Test
     public void getId() throws Exception {
-
+        assert(p.getId() == PLAYER_ID);
     }
 
     @Test
     public void toStringTest() throws Exception {
-
+        assert(p.toString().equals(TO_STRING));
     }
 
     @Test
     public void equals() throws Exception {
-
+        Player p2 = new DefaultPlayer(PLAYER_ID, PLAYER_NAME);
+        assert(p.equals(p2));
     }
 
 }
